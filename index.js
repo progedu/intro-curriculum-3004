a63bcc5f768b728e1cf75b9711cad135a8979025
Chagi <amenbows@yahoo.co.jp>
2016-09-09 04:19:23 +0900

    multi 関数を追加

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 'use strict';
+/**
+	* 配列で渡された整数を全て足しあわせて返す関数
+	* @param {number[]} numbers  - 配列で渡された整数
+	* @return {number} result  - 足しあわせた結果の値
+*/
 function add(numbers) {
     let result = 0;
     for (let num of numbers) {
@@ -6,6 +11,20 @@ function add(numbers) {
     }
     return result;
 }
+/**
+	* 配列で渡された整数を全て掛けあわせて返す関数
+	* @param {number[]} numbers  - 配列で渡された整数
+	* @return {number} result  - 掛けあわせた結果の値
+*/
+function multi(numbers) {
+	let result = 1;
+	for (let num of numbers) {
+		result = result * num;
+	}
+	return result;
+}
+
 module.exports = {
-    add: add
+    add: add,
+    multi: multi
 };
