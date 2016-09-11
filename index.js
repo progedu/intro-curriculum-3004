850307ea44ab76ddbef14d8c95fd1df57825d23f
sesamig <opensesamig@yahoo.co.jp>
2016-09-11 19:30:42 +0000

    multi 関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict';
+
 function add(numbers) {
     let result = 0;
     for (let num of numbers) {
@@ -6,6 +7,18 @@ function add(numbers) {
     }
     return result;
 }
-module.exports = {
-    add: add
+
+
+function multi(numbers) {
+    let result = 1;
+    for (let num of numbers) {
+        result = result * num;
+    }
+    return result;
+}
+
+// 外から使えるようにする
+module.exports = { // オブジェクト
+    add: add,
+    multi: multi
 };
