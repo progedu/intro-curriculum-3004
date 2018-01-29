0280808de943b312654a7f64a286119b93069d9a
TrunkRoom <none@none.none>
2018-01-29 07:58:37 +0000

    multi関数の追加

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 'use strict';
+
+// add　配列の中の整数をすべてを足し合わせる
+// multi　配列の中の整数をすべて掛け合わせる
+
 function add(numbers) {
     let result = 0;
     for (let num of numbers) {
@@ -6,6 +10,17 @@ function add(numbers) {
     }
     return result;
 }
+
+function multi(numbers) {
+    let result = 1;
+    for (let num of numbers) {
+        result = result * num;
+    }
+    return result;
+}
+
+
 module.exports = {
-    add: add
+    add: add,
+    multi: multi
 };
