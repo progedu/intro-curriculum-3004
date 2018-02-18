2e761c7cc2fc9207dbfdb605e2727fa472adfe9d
enmto <y.enmto@gmail.com>
2018-02-18 06:13:59 +0000

    ’加えての部分の追加’addでもmultiでも表示されるように実装

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
+function add(numbers) {
+    let result = 0;
+    for (let num of numbers) {
+        result = result + num;
+    }
+    return result;
+};
+
 function multi(numbers) {
     let i = 1;
     for (let num of numbers){
@@ -7,5 +15,6 @@ function multi(numbers) {
     return i;
 }
 module.exports = {
-    multi: multi
+    add : add,
+   multi: multi
 };
