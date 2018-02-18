22a9dc1e4dd017debd531a52a83925f4624699ee
enmto <y.enmto@gmail.com>
2018-02-18 06:10:00 +0000

    multiで配列をかけるように実装

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict';
-function add(numbers) {
-    let result = 0;
-    for (let num of numbers) {
-        result = result + num;
+function multi(numbers) {
+    let i = 1;
+    for (let num of numbers){
+        i = i * num;
     }
-    return result;
+    return i;
 }
 module.exports = {
-    add: add
+    multi: multi
 };
