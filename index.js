ecb7d89490aded98be0150195931b3be051b00d7
newwave8 <new.wave.hatch@gmail.com>
2018-05-03 03:55:58 +0000

    sumパッケージにmulti関数を実装

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 'use strict';
-function add(numbers) {
+function add(numbers) { // 引数numbersは配列を受け取る
     let result = 0;
     for (let num of numbers) {
         result = result + num;
     }
     return result;
 }
+
+function add(numbers) {
+    let result = 1;
+    for (let num of numbers) {
+        result = result * num;
+    }
+    return result;
+}
 module.exports = {
-    add: add
+    add : add,
+    multi : multi
 };
