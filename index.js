51b1b47a05e933424f3ed6f1f9532928a25e804b
kanpachi1000 <antomo10@sf.commufa.jp>
2018-05-12 11:10:45 +0000

    multi課題

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ function add(numbers) {
     }
     return result;
 }
+
+function multi(numbers) {
+    let result2 = 1;
+    for (let num of numbers) {
+        result2 = result2 * num;
+    }
+    return result2;
+}
+
+
 module.exports = {
     add: add
+};
+
+module.exports = {
+    multi: multi
 };
