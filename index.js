e6f5f7ea18dd1acb7a3e659ff3ff4dd88497daa5
kenkoumoto <kenkou0306@gmail.com>
2018-05-29 00:03:18 +0000

    かけ合わせるmulti関数を追加

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict';
+
 function add(numbers) {
     let result = 0;
     for (let num of numbers) {
@@ -6,6 +7,15 @@ function add(numbers) {
     }
     return result;
 }
+
+function multi(numbers) {
+    let result = 1;
+    for (let num of numbers) {
+        result = result * num;
+    }
+    return result;
+}
 module.exports = {
-    add: add
+    multi: multi,
+    add:add
 };
