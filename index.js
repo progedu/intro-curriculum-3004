9e60f592042bda2934b14c7b46585bde6ddea34c
yubari26 <38066470+yubari26@users.noreply.github.com>
2018-05-31 05:55:20 +0900

    multiを追加

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ function add(numbers) {
     }
     return result;
 }
+
+function multi(numbers) {
+    let result = 0;
+    for (let num of numbers) {
+        result = result * num;
+    }
+    return result;
+}
+
 module.exports = {
     add: add
+    multi: multi
 };
