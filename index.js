b43de508445e51f459fb658a64cdf1b8714ff230
carlitowiu <youllneverwalkalone0817@gmail.com>
2018-06-19 10:02:42 +0000

    コメントアウトも追加して実装

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict';
+// 引数numbersで受け取った数を、その数分だけ足し合わせる
 function add(numbers) {
     let result = 0;
     for (let num of numbers) {
@@ -6,6 +7,7 @@ function add(numbers) {
     }
     return result;
 }
+// 引数numbersで受け取った数を、その数分だけ掛け合わせる
 function multi(numbers) {
     let result = 1;
     for (let num of numbers) {
@@ -13,6 +15,7 @@ function multi(numbers) {
     }
     return result;
 }
+// 関数を他の所でも使えるようにエキスポートする
 module.exports = {
     add: add,
     multi: multi
