ac51b1f4e25f69cbee17c5eba71ef30718d35a23
nicozon <pent_mikot@yahoo.co.jp>
2018-09-05 03:35:47 +0900

    掛け合わせる multi 関数の実装

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 'use strict';
+// addモジュール
 function add(numbers) {
     let result = 0;
-    for (let num of numbers) {
+    for(let num of numbers) {
         result = result + num;
     }
     return result;
 }
+// multiモジュール
+function multi(numbers) {
+    let result = 1;
+    for(let num of numbers) {
+        result = result * num;
+    }
+    return result;
+}
+// 他のモジュールからでもアクセスできる
 module.exports = {
-    add: add
-};
+    add: add,
+    multi: multi
+}
