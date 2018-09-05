cb781dab7bac53f98aa3b56c388100517c8278bc
oriyoshi <yorishio.yoshio@gmail.com>
2018-09-05 05:49:04 +0000

    引き算、掛け算、割り算の関数を実装

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict';
+//足し算
 function add(numbers) {
     let result = 0;
     for (let num of numbers) {
@@ -6,6 +7,41 @@ function add(numbers) {
     }
     return result;
 }
+//引き算
+function sub(numbers) {
+    let result = 0;
+    for (let num of numbers) {
+        result = result - num;
+    }
+    return result;
+}
+//掛け算
+function multi(numbers) {
+    let result = 0;
+    for (let num of numbers) {
+	    if(result === 0){
+		    result = num;
+	    }else{
+        	result = result * num;
+	    }    
+	}
+    return result;
+}
+//割り算
+function div(numbers) {
+    let result = 0;
+    for (let num of numbers) {
+        if(result === 0){
+		    result = num;
+	    }else{
+        	result = result / num;
+	    }  
+    }
+    return result;
+}
 module.exports = {
-    add: add
+    add : add,
+    sub : sub,
+    multi : multi,
+    div : div
 };
