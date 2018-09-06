b1cb640a2706d62bd6dc63c10d435d421918a182
umihime <core.editor>
2018-09-06 16:11:27 +0000

    multi関数の実装

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ function add(numbers) {
     return result;
 }
 
-function multi(nubers) {
+function multi(numbers) {
     let result = 1;
-    for (let num of nubers) {
+    for (let num of numbers) {
         result = result * num;
     }
+    return result
 }
 
 module.exports = {
