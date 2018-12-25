5ed1d4ac49c3c6be8b3c2eb29422bd916a6e9cf9
Seiichiro Kizaki <biophylaxis@gmail.com>
2018-12-25 12:37:58 +0000

    multiply

--- index.js
+++ index.js
@@ -6,6 +6,30 @@ function add(numbers) {
     }
     return result;
 }
+function multi(numbers) {
+    let result = 1;
+    for (let num of numbers) {
+        result = result * num;
+    }
+    return result;
+}
+function div100(numbers) {
+    let result = 100;
+    for (let num of numbers) {
+        result = result / num;
+    }
+    return result;
+}
+function div(numbers) {
+    let result = numbers[0];
+    for (let i = 1; i < numbers.length; i++) {
+        result = result / numbers[i];
+    }
+    return result;
+}
 module.exports = {
-    add: add
+    add : add,
+    multi : multi,
+    div100 : div100,
+    div : div
 };
