feb584f49af6a97f8a7c9f99bb3863e82d51e6ca
take <thibodeau9@gmail.com>
2019-01-15 13:52:19 +0000

    add multi function

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-'use strict';
+"use strict";
 function add(numbers) {
-    let result = 0;
-    for (let num of numbers) {
-        result = result + num;
-    }
-    return result;
+  let result = 0;
+  for (let num of numbers) {
+    result = result + num;
+  }
+  return result;
 }
+
+const multi = numbers => numbers.reduce((x, y) => x * y);
+// console.log(multi([4, 5, 6]) === 120);
+
 module.exports = {
-    add: add
+  add: add,
+  multi: multi
 };
