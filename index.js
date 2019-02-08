91bf8e21b0a6938491b38654a089bc9101f55203
toryo306 <46623910+toryo306@users.noreply.github.com>
2019-02-09 01:06:41 +0900

    multi関数を追加

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 'use strict';
-function add(numbers) {
+function add(numbers){
     let result = 0;
-    for (let num of numbers) {
+    for (let num of numbers){
         result = result + num;
     }
     return result;
 }
+
+'use strict';
+function multi(numbers){
+    let result = 1;
+    for (let num of numbers){
+        result = result * num;
+    }
+    return result;
+}
+
 module.exports = {
-    add: add
+    add : add,
+    multi : multi
 };
