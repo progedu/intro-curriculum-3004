bc90a24607fd6c0dc30a90e84e432d7365f27854
takuyaamamo <40745102+takuyaamamo@users.noreply.github.com>
2019-04-15 10:28:08 +0900

    コメントと追記multiたいおう

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict';
+// 配列で渡された整数を全て足す関数
 function add(numbers) {
     let result = 0;
     for (let num of numbers) {
@@ -6,6 +7,15 @@ function add(numbers) {
     }
     return result;
 }
+// 配列で渡された整数を全てかける関数
+const multi = numbers => {
+    let result = 1;
+    for (let num of numbers) {
+      result = result * num;
+    }
+    return result;
+};
 module.exports = {
-    add: add
+    add: add,
+  multi : multi
 };
