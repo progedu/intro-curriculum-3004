1ea3e9c92cfaa9433168fdb3d19b195f0ff392a9
guricerin <gurizumi@gmail.com>
2019-04-19 16:01:24 +0000

    add multi function

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 'use strict';
+
 function add(numbers) {
     let result = 0;
     for (let num of numbers) {
-        result = result + num;
+        result += num;
     }
     return result;
 }
+
+function multi(numbers) {
+    let res = 1;
+    for (const num of numbers) {
+        res *= num;
+    }
+    return res;
+}
+
 module.exports = {
-    add: add
+    add : add,
+    multi : multi
 };
