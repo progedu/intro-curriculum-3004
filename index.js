ee210ee9e3df91a14f84d502b062cf2373e6b54f
AyasaFushimi <bolbus0617@gmail.com>
2019-05-16 21:53:58 +0900

    multi関数の追加

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 'use strict';
-function add(numbers) {
-  let result = 0;
-  for (let num of numbers) {
-    result = result + num;
-  }
-  return result;
+function add(numbers){
+    let result = 0;
+    for(let num of numbers){
+        result = result + num;
+    }
+    return result;
 }
-module.exports = { add };
+function multi(numbers){
+    let result = 1;
+    for(let num of numbers){
+        result = result * num;
+    }
+    return result;
+}
+module.exports = { add, multi};
