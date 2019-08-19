e06659af3a1f468cb7e6f398446fabdd200d4ab4
Taisuke Kawamata <kmwt810@gmail.com>
2019-08-19 10:53:27 +0000

    乗算とテストの追加

--- index.js
+++ index.js
@@ -6,4 +6,17 @@ function add(numbers) {
   }
   return result;
 }
-module.exports = { add };
+function multi(numbers) {
+  let result = 1;
+  for (let num of numbers) {
+    result = result * num;
+  }
+  return result;
+}
+// function multiTest() {
+//   const assert = require("assert");
+//   assert.equal(multi([1, 2, 3, 4]), 10, `multi([1, 2, 3, 4]=10, but ${multi([1, 2, 3, 4])}`);
+//   assert.equal(multi([1, 2, 3, 4]), 24, `multi([1, 2, 3, 4]=24, but ${multi([1, 2, 3, 4])}`);
+// }
+// test();
+module.exports = { add, multi };
