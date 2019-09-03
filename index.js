18f0e935d81704312ce1bfad63872a72ac1d2659
あさづけ <quusan3@gmail.com>
2019-09-03 19:55:05 +0900

    multi関数+assert追加

--- index.js
+++ index.js
@@ -6,4 +6,18 @@ function add(numbers) {
   }
   return result;
 }
-module.exports = { add };
+
+function multi(numbers) {
+  let result = 1;
+  for (let num of numbers) {
+    result = result * num;
+  }
+  return result;
+}
+
+module.exports = { add,multi };
+
+const assert = require('assert');
+assert.equal(add([1,2,3,4]), 10, `error:値が違います 出力された値：${add([1,2,3,4])}`);
+assert.equal(multi([1,2,3,4]), 24,  `error:値が違います 出力された値：${multi([1,2,3,4])}`);
+console.log('すべてのテストを通過しました');
