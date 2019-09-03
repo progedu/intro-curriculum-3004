0ce509737c0586bf4cf1fc0db1ce31255c984c56
TOUEi10 <52106355+touei10@users.noreply.github.com>
2019-09-03 22:01:36 +0900

    渡せれた配列の整数を全てかけ合わせる multi()

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 'use strict';
 function add(numbers) {
-  let result = 0;
-  for (let num of numbers) {
-    result = result + num;
-  }
-  return result;
+    let result = 0;
+    for (let num of numbers) {
+        result = result + num;
+    }
+    return result;
 }
-module.exports = { add };
+
+function multi(numbers) {
+    let result = 1;
+    for (let num of numbers) {
+        result = result * num;
+    }
+    return result;
+}
+
+console.log(add([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+console.log(multi([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+
+
+module.exports = {
+    add: add,
+    multi: multi
+};
