36719279541c653bae4494f6e109377b52a48f75
Hatsunori Kashima <hatsunri.kashima@gmail.com>
2019-10-24 06:42:02 +0900

    multiの実装

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 'use strict';
 function add(numbers) {
-  let result = 0;
-  for (let num of numbers) {
-    result = result + num;
-  }
-  return result;
+    let result = 0;
+    for (let num of numbers) {
+        result = result + num;
+    }
+    return result;
 }
-module.exports = { add };
+
+function multi(numbers) {
+    let result = 1;
+    if (!numbers) {
+        return 0;
+    }
+    for (let num of numbers) {
+        result = result * num;
+    }
+    return result;
+}
+
+module.exports = {
+    add: add,
+    multi: multi
+};
+
+console.assert(multi([1, 2, 5]) === 10, "掛け算の結果が正しくありません:" + "expected: 10, but got " + multi([1, 2, 5]));
+console.assert(multi() === 0, "空入力に対して答えが0ではありません")
