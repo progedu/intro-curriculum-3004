726d75ce2aac59a7da9afe051fbb2c39487ffe57
daisukeya <yamadaisuke0922@gmail.com>
2019-11-07 03:57:46 +0900

    multi作りました。

--- index.js
+++ index.js
@@ -6,4 +6,14 @@ function add(numbers) {
   }
   return result;
 }
+
+function multi(numbers) {
+  let result = 0;
+  for (let num of numbers) {
+    result *= num;
+  }
+  return result;
+}
+
 module.exports = { add };
+module.exports = { multi };
