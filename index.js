2bde3e5bff3289afd9dac9d8ebf24d4fdd2284ae
moto1985 <moto19851218@gmail.com>
2019-11-10 20:08:31 +0900

    multi関数を追加

--- index.js
+++ index.js
@@ -6,4 +6,13 @@ function add(numbers) {
   }
   return result;
 }
-module.exports = { add };
+
+function multi(numbers) {
+  let result = 1;
+  for (let num of numbers) {
+    result = result * num;
+  }
+  return result;
+}
+
+module.exports = { add, multi };
