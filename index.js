e97c70865313ba186182545bc60cda10ea49663c
koshi <t7o0m5@gmail.com>
2019-12-29 22:59:48 +0900

    multi関数の修正

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ function add(numbers) {
   }
   return result;
 }
+
 function multi(numbers) {
-  let result = 0;
+  let result = 1;
   for (let num of numbers) {
     result = result * num;
   }
   return result;
 }
-module.exports = { add };
-module.exports = { multi };
+module.exports = { add, multi };
