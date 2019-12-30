181b833725b70f0f919ac8381b3f98cafb8f4df7
浜田行徳 <gyohtoku@icloud.com>
2019-12-30 14:37:36 +0900

    multiパッケージ作成

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict';
-function add(numbers) {
+function multi(numbers) {
   let result = 0;
   for (let num of numbers) {
-    result = result + num;
+    result = result * num;
   }
   return result;
 }
-module.exports = { add };
+module.exports = { multi };
