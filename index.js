188d1715da47f3801a4af7e00cea89921292f184
hashimoto <h.hashimoto.web@gmail.com>
2020-01-29 04:30:11 +0000

    add multi

--- index.js
+++ index.js
@@ -6,4 +6,17 @@ function add(numbers) {
   }
   return result;
 }
-module.exports = { add };
+
+function multi(numbers) {
+  let result = 1;
+  for (let num of numbers) {
+    result = result * num;
+  }
+  return result;
+}
+
+module.exports = { add,multi };
+
+// module.exports = { add };
+// module.exports = { multi };
+//上記の書き方だとmultiしか動かず気がつくのに少し時間がかかりました
