7631363995de80c2746288654c2cce31575121fc
awyaki <awyakitaka@gmail.com>
2020-02-06 13:37:11 +0000

    multi function was deployed

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 'use strict';
-function add(numbers) {
+
+let add = (numbers) => {
   let result = 0;
   for (let num of numbers) {
     result = result + num;
   }
   return result;
 }
-module.exports = { add };
+
+let multi = (numbers) => {
+  let result = 1;
+  for (let num of numbers) {
+    result = result * num;
+  }
+  return result;
+};
+
+module.exports = { add, multi };
