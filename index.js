ab2da8de192b14301383f3482c799edcb49a0018
Yapin <k-yao_0308@yapin-et-creation.com>
2020-02-23 22:09:26 +0900

    multi関数の実装

--- index.js
+++ index.js
@@ -6,4 +6,17 @@ function add(numbers) {
   }
   return result;
 }
-module.exports = { add };
+module.exports = {
+  add: add
+};
+
+function multi(numbers) {
+  let result = 0;
+  for (let num of numbers) {
+    result = result * num;
+  }
+  return result;
+}
+module.exports = {
+  multi: multi
+};
