9015aae550734405bed56a61de3a351064736a68
sheLIAkk <sheliask252@gmail.com>
2020-03-19 07:31:37 +0900

    multiの実装

--- index.js
+++ index.js
@@ -6,13 +6,12 @@ function add(numbers) {
   }
   return result;
 }
-module.exports = { add };
 
-function multi(n) {
-  let result = 0;
-  for (let number of n) {
-    result = result * number;
+function multi(numbers) {
+  let result = 1;
+  for (let num of numbers) {
+    result = result * num;
   }
   return result;
 }
-module.exports = { multi };
+module.exports = { add, multi };
