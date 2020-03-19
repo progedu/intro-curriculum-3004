8f1d0fafcc239af3d090ccee822036c13a7babf4
Akihiro Izumikawa <a.izumikawa@gmail.com>
2020-03-20 05:30:02 +0900

    test

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function add(numbers) {
   }
   return result;
 }
+
 function multi(numbers) {
   let result = 1;
   for (let num of numbers) {
@@ -13,4 +14,5 @@ function multi(numbers) {
   }
   return result;
 }
+
 module.exports = { add,multi };
