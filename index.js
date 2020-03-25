6deacd6b875ab400308417e6140ece25bd22548a
Jinta Fujino <fjn815@icloud.com>
2020-03-25 17:44:55 +0900

    関数の追加

--- index.js
+++ index.js
@@ -6,4 +6,15 @@ function add(numbers) {
   }
   return result;
 }
-module.exports = { add };
+
+function multi(numbers) {
+  let result = 1;
+  for (let num of numbers) {
+    result = result * num;
+  }
+  return result
+}
+module.exports = {
+  add : multi
+};
+console.log(multi([1, 2, 3, 4]));
