2008bf8f00d9f668ade6b944907cbdb2f2a3a66d
Kan Tachibana <tachibanaa1211@gmail.com>
2020-03-31 19:39:16 +0900

    multi関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
+function multi(numbers) {
+  let result = 0;
+  for (let num of numbers) {
+    result = result * num;
+  }
+  return result;
+}
+
 function add(numbers) {
   let result = 0;
   for (let num of numbers) {
@@ -6,4 +14,7 @@ function add(numbers) {
   }
   return result;
 }
-module.exports = { add };
+module.exports = {
+  multi, add
+}
+
