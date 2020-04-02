791f0b5b0f97775e0cdc72a0ac5b31879c2e39e7
maiamea <maiameawork@gmail.com>
2020-04-02 15:30:03 +0900

    multi関数追加

--- index.js
+++ index.js
@@ -6,4 +6,14 @@ function add(numbers) {
   }
   return result;
 }
-module.exports = { add };
+// module.exports = { add };
+
+// 配列で渡された 整数を全て掛けあわせて返す multi 関数
+function multi(numbers) {
+  let result = 1;
+  for (let num of numbers) {
+    result = result * num;
+  }
+  return result;
+}
+module.exports = { add, multi };
