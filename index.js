cfcbb267825df64db84518d63be0eef79e4c162e
anazsama <anazsama@outlook.jp>
2020-04-22 06:06:04 +0900

    順次掛け合わせの関数

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-'use strict';
-function add(numbers) {
-  let result = 0;
-  for (let num of numbers) {
-    result = result + num;
-  }
-  return result;
+'use strict'
+function add(numbers){
+    let result = 0;
+    for (let num of numbers){ //配列numbersの要素数分ループし、先頭要素から順に取り出す
+        result = result + num;
+    }
+    return result;
 }
-module.exports = { add };
+
+function multi(numbers){
+    let result = 0;
+    for (let num of numbers){ //配列numbersの要素数分ループし、先頭要素から順に取り出す
+        console.log('!num:' + num +'/result:' + result);
+        if(!result){ //resultが初期値=0の場合のみ、num値をセット
+            result = num;
+        }else{
+            result = result * num;
+        }
+    }
+    return result;
+}
+
+module.exports = {
+    add : add,
+    multi : multi
+}
