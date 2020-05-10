198629f71dcb2fdb3e35a494b857895426781fc9
mican-wan2 <nyn3.moco@gmail.com>
2020-05-10 14:16:22 +0900

    multiメソッド追加

--- index.js
+++ index.js
@@ -6,4 +6,14 @@ function add(numbers) {
   }
   return result;
 }
+
+function multi(numbers){
+  let result = 1;
+  for(let num of numbers){
+    result = result * num;
+  }
+  return result;
+}
+
 module.exports = { add };
+module.exports = {multi};
