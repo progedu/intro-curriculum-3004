a0c0f0fe917952731a82b69e081c2d0b01c4b40f
Tomoya Kanemaru <pandora.border@gmail.com>
2020-05-18 21:13:44 +0900

    整数をすべて掛け合わせて戻すmulti関数を追加

--- index.js
+++ index.js
@@ -6,4 +6,14 @@ function add(numbers) {
   }
   return result;
 }
-module.exports = { add };
+
+function multi(numbers) {
+  let result = 1;
+  for (let num of numbers) {
+    result = result * num;
+  }
+  return result;
+}
+
+// 複数をexportする場合は、並べて定義する必要がある
+module.exports = { add, multi };
