fc14baef2ad94b85b765acc6ef6fbc448f48a15c
yakata66 <ponyo4869yakata398@gmail.com>
2020-05-24 16:08:07 +0900

    implement multi function.

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 function add(numbers) {
   let result = 0;
   for (let num of numbers) {
-    result = result + num;
+    result = result * num;
   }
   return result;
 }
-module.exports = { add };
+module.exports = { multi };
