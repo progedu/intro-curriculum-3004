c5899467ff786820324bf524578e56700afe4ae2
Ryuji Yamamoto <yamamoto.ryuji.dev@gmail.com>
2020-05-24 17:33:48 +0900

    掛け算を行う関数を追加

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict';
+
 function add(numbers) {
   let result = 0;
   for (let num of numbers) {
@@ -6,4 +7,16 @@ function add(numbers) {
   }
   return result;
 }
-module.exports = { add };
+
+function multi(numbers) {
+  let result = 1;
+  for (const num of numbers) {
+    result = result * num;
+  }
+  return result;
+}
+
+module.exports = {
+  add,
+  multi
+};
