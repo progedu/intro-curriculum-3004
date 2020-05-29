eee302dbc3c0b0381e9f5b732f7533305400cfe8
henohenon <takeru_20n1100821@nnn.ed.jp>
2020-05-29 09:22:56 +0900

    そういうことだったのか

--- index.js
+++ index.js
@@ -2,8 +2,15 @@
 function add(numbers) {
   let result = 1;
   for (let num of numbers) {
+    result = result + num;
+  }
+  return result;
+}
+function multi(numbers) {
+  let result = 1;
+  for (let num of numbers) {
     result = result * num;
   }
   return result;
 }
-module.exports = { add };
+module.exports = { add,multi };
