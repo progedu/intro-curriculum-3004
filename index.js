03130131e7169bbe929d48ad7370314dcd43e718
SakiArai <saki.arai.oden@gmail.com>
2020-06-22 17:04:26 +0900

    multi関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
+function multi(numbers) {
+    let result = 1;
+    for (let num of numbers) {
+        result = result * num;
+    }
+    return result;
+}
+
 function add(numbers) {
     let result = 0;
     for (let num of numbers) {
@@ -7,5 +15,5 @@ function add(numbers) {
     return result;
 }
 module.exports = {
-    add: add
+    add, multi
 };
