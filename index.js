09434485d974b351c02f6057ef0a5a79e1c21bd3
UPLGHR <upperlip1130@gmail.com>
2020-07-04 21:01:02 +0900

    ライブラリの練習問題

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict';
+
 function add(numbers) {
   let result = 0;
   for (let num of numbers) {
@@ -6,4 +7,22 @@ function add(numbers) {
   }
   return result;
 }
-module.exports = { add };
+
+function multi(numbers) {
+  let result = 0;
+  
+  for (let i = 0; i < numbers.length; i++)
+  {
+    if (i === 0){
+      result = numbers[i];
+    }
+    else
+    {
+      result = result * numbers[i];
+    }
+  }
+  
+  return result;
+}
+
+module.exports = { add, multi };
