c513e6bf39790217f7c0c8349531b315551191bd
Tomoharu yuyama <tomoharuyuyama@gmail.com>
2020-07-06 00:35:07 +0900

    practice4

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ function add(numbers) {
   return result;
 }
 module.exports = { add };
+function multi(numbers) {
+    let result = 1;
+    for (let num of numbers) {
+      result = result * num;
+    }
+    return result;
+  }
+  
+  module.exports = { add, multi };
