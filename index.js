44dba19e31e22c0cc055de6f52afe9f5af4834f8
工藤昂大 <kodai_kudo@bita.jp>
2020-07-14 17:06:12 +0900

    mutiを実装完了

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict';
-function add(numbers) {
-  let result = 0;
-  for (let num of numbers) {
-    result = result + num;
+function multi(array) {
+  let result = 1;
+  for (let num of array) {
+    result = result * num;
   }
   return result;
 }
-module.exports = { add };
+module.exports = { multi };
