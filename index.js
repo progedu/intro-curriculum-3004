0b83a868b9a69aa83a1d5123bc453e7808be84e4
kamehei713 <mkyoro713@gmail.com>
2020-07-17 16:46:24 +0900

    multi関数の追加

--- index.js
+++ index.js
@@ -7,13 +7,11 @@ function add(numbers) {
   return result;
 }
 function multi(numbers){
-  let result = 0;
+  let result = 1;
   for (let num of numbers){
     result *= num;
   }
+  return result;
 }
 
-module.exports = {
-  add:add,
-  multi:multi,
-};
+module.exports = {add,multi};
