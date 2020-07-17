ebf5e96cbf6c1a141b64246b425ed4d026ae30c0
kamehei713 <mkyoro713@gmail.com>
2020-07-17 16:58:43 +0900

    a

--- index.js
+++ index.js
@@ -6,12 +6,6 @@ function add(numbers) {
   }
   return result;
 }
-function multi(numbers){
-  let result = 1;
-  for (let num of numbers){
-    result *= num;
-  }
-  return result;
-}
+
 
 module.exports = {add,multi};
