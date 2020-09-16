ba9631155a1518d09626449e6471a103924c8371
Aozora Okada <bluesky125osmkk@gmail.com>
2020-09-16 17:06:12 +0900

    pow関数を追加

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 'use strict';
+/**
+ * 配列の中身をすべて足し算して数値で返却します
+ * @param {Array} numbers
+ * @returns {Number}
+ */
 function add(numbers) {
   let result = 0;
   for (let num of numbers) {
@@ -14,5 +19,16 @@ function multi(numbers) {
   }
   return result;
 }
-
-module.exports = { add, multi };
+/**
+ * 配列のべき乗を計算
+ * @param {Array} numbers 正の整数の配列
+ * @return {number}
+ */
+function pow(numbers ) {
+  let result = 1;
+  for (let num of numbers) {
+    result = result ** num;
+  }
+  return result;
+}
+module.exports = { add, multi, pow };
