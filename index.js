7eaf8e6a7624452439f32f5b4213c271f9de18a7
Yuya Satake <sata.yu.7878@gmail.com>
2020-10-28 19:36:04 +0900

    modified sum function

--- index.js
+++ index.js
@@ -6,4 +6,15 @@ function add(numbers) {
   }
   return result;
 }
-module.exports = { add };
+
+function multi(numbers) {
+  let result = 1;
+  for (let num of numbers) {
+    result = reasult * num;
+  }
+  return result;
+}
+module.exports = {
+  add,
+  multi
+};
