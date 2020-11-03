eff792e371fb741c43041f28897657ece2fc29dd
masa1993 <70550130+masa1993@users.noreply.github.com>
2020-11-03 10:42:23 +0900

    multiの定義

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ function add(numbers) {
   return result;
 }
 module.exports = { add };
+
+function multi(numbers) {
+  let result = 1;
+  for (let num of numbers) {
+    resutl = result * num;
+    }
+    return result;
+    }
+module.exports = {
+    add, multi };
