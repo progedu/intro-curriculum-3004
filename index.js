2a6eb2e1eacba33e182cbcda629c8ad2f6868730
proglawm <proglawm@gmail.com>
2021-01-04 06:09:07 +0900

    multi関数を定義

--- index.js
+++ index.js
@@ -6,4 +6,17 @@ function add(numbers) {
   }
   return result;
 }
-module.exports = { add };
+function multi(numbers) {
+  if (numbers.length === 0) {
+    return;
+  }
+  let result = 1;
+  for (let num of numbers) {
+    result = result * num;
+  }
+  return result;
+}
+module.exports = {
+  add,
+  multi,
+};
