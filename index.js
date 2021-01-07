2f32e715a2f2b4fea4b2b191684fda3cbdf37259
aozametatuki <akomatsu1224@gmail.com>
2021-01-07 11:58:59 +0900

    multi関数の追記

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict';
+
 function add(numbers) {
   let result = 0;
   for (let num of numbers) {
@@ -6,4 +7,15 @@ function add(numbers) {
   }
   return result;
 }
-module.exports = { add };
+function multi(numbers){
+  let result = 1;
+  for(let num of numbers){
+      result = result * num;
+  }
+  return result;
+}
+
+module.exports = {
+  add : add,
+  multi: multi
+};
