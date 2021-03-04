44cbac3f549d950ac5c4f4c385f34c41c22ad46a
kumo-kabe <alcea.pleiades@gmail.com>
2021-03-04 16:45:16 +0900

    multi関数を実装

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict';
+// 配列で渡された整数を全て足し合わせる
 function add(numbers) {
   let result = 0;
   for (let num of numbers) {
@@ -6,4 +7,12 @@ function add(numbers) {
   }
   return result;
 }
-module.exports = { add };
+// 配列で渡された整数を全てかけ合わせる
+function multi(numbers) { 
+  let result = 1;
+  for (let num of numbers) { 
+    result = result * num;
+  }
+  return result;
+}
+module.exports = { add,multi };
