bc30a55b53d7299450afa2386d00858bbd0373b8
Aoi Yamaguchi <yamaguchiaoi0130@gmail.com>
2021-03-12 17:24:16 +0900

    修正

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 'use strict';
 function add(numbers) {
   let result = 0;
@@ -6,4 +7,13 @@ function add(numbers) {
   }
   return result;
 }
-module.exports = { add };
+
+function multi(numbers) {
+  let result = 1;
+  for (let num of numbers) {
+    result = result * num;
+  }
+  return result;
+}
+
+module.exports = { add, multi };
