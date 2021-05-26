771bdc678ab21a58db7d8220caed20af5fa97502
tobihiro <tobihiro4545@gmail.com>
2021-05-26 12:42:45 +0900

    add,multiモジュール作成

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 'use strict';
-function add(numbers) {
+function add(numbers){
     let result = 0;
     for (let num of numbers) {
         result = result + num;
     }
     return result;
 }
+function multi(numbers){
+    let result = 0;
+    for (let num of numbers){
+        result = result * num;
+    }
+}
 module.exports = {
-    add: add
+    add,
+    multi
 };
